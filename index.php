d2a9fbbf4554e3405b5bb6c2f62cd13f81d78e29
Thomas <oldefaronkel@gmail.com>
2022-01-06 21:47:01 +0100

    Reroll :D

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<style>
+	html,
+	body,
+	h1,
+	h2,
+	h3,
+	h4,
+	h5 {
+		font-family: "Raleway", sans-serif
+	}
+    
+</style>
+<body class="w3-light-grey">
+
+
+	<div class="w3-bar w3-top w3-teal w3-large">
+		<span class="w3-bar-item w3-right">Logo</span>
+	</div>
+
+<div class="w3-display-middle">
+<form class="w3-container w3-card-4">
+<p>
+<input class="w3-input" type="text" style="width:90%" required>
+<label>Name</label></p>
+<p>
+<input class="w3-input" type="password" style="width:90%" required>
+<label>Password</label></p>
+
+
+<p>
+<button class="w3-button w3-section w3-teal w3-ripple"> Log in </button></p>
+
+</form>
+
+</div>
+
+</body>
+</html> 
